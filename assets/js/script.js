a5917186d60e2db00c79c3b93e8d20cd8ae25949
Kate McGuane <katemcguane@hotmail.com>
2024-02-06 17:09:26 +0000

    Adjust spelling error in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
+const cardData = [
+    {
+        'pairNumber': 1,
+        'image': 'assets/images/card-images/bread-en.png',
+        'alt': 'image of a loaf of bread',
+        'words': 'bread',
+      },
+      {
+        'pairNumber': 1,
+        'image': 'assets/images/card-images/bread-tg.png',
+        'alt': 'image of a loaf of bread; translates to tinapay in tagalog',
+        'words': 'tinapay',
+      },
+      {
+        'pairNumber': 2,
+        'image': 'assets/images/card-images/fruit-en.png',
+        'alt': 'image of a bowl of fruit',
+        'words': 'fruit',
+      },
+      {
+        'pairNumber': 2,
+        'image': 'assets/images/card-images/fruit-tg.png',
+        'alt': 'image of a bowl of fruit; translates to prutas in tagalog',
+        'words': 'prutas',
+      },
+      {
+        'pairNumber': 3,
+        'image': 'assets/images/card-images/fish-en.png',
+        'alt': 'image of a cooked fish',
+        'words': 'fish',
+      },
+      {
+        'pairNumber': 3,
+        'image': 'assets/images/card-images/fish-tg.png',
+        'alt': 'image of a cooked fish; translates to isda in tagalog',
+        'words': 'isda',
+      },
+      {
+        'pairNumber': 4,
+        'image': 'assets/images/card-images/egg-en.png',
+        'alt': 'image of an egg',
+        'words': 'egg',
+      },
+      {
+        'pairNumber': 4,
+        'image': 'assets/images/card-images/egg-tg.png',
+        'alt': 'image of an egg; translates to itlog in tagalog',
+        'words': 'itlog',
+      },
+]
+
+
 let cards = document.querySelectorAll('.card-box');
 //click card function
 
