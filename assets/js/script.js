d3fc5ee9c5ef550ef0c6a3ca5c8c548fec75d75e
Kate Mcguane <katemcguane@hotmail.com>
2024-09-16 10:22:35 +0000

    Add DOMContentLoaded event listener to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,132 +1,135 @@
-// Card Data Array - with our card objects including pair, image, alt & words
-const cardArray = [
-  {
-    'pair': 1,
-    'image': 'assets/images/card-images/bread-en.png',
-    'alt': 'image of a loaf of bread',
-    'word': 'bread'
-  },
-  {
-    'pair': 1,
-    'image': 'assets/images/card-images/bread-tg.png',
-    'alt': 'image of a loaf of bread; translates to "tinapay" in tagalog',
-    'word': 'tinapay'
-  },
-  {
-    'pair': 2,
-    'image': 'assets/images/card-images/egg-en.png',
-    'alt': 'image of an egg',
-    'word': 'egg'
-  },
-  {
-    'pair': 2,
-    'image': 'assets/images/card-images/egg-tg.png',
-    'alt': 'image of an egg; translates to "itlog" in tagalog',
-    'word': 'itlog'
-  },
-  {
-    'pair': 3,
-    'image': 'assets/images/card-images/fish-en.png',
-    'alt': 'image of a cooked fish',
-    'word': 'fish'
-  },
-  {
-    'pair': 3,
-    'image': 'assets/images/card-images/fish-tg.png',
-    'alt': 'image of a cooked fish; translates to "isda" in tagalog',
-    'word': 'isda'
-  },
-  {
-    'pair': 4,
-    'image': 'assets/images/card-images/fruit-en.png',
-    'alt': 'image of a bowl of fruit',
-    'word': 'fruit'
-  },
-  {
-    'pair': 4,
-    'image': 'assets/images/card-images/fruit-tg.png',
-    'alt': 'image of a bowl of fruit; translates to "prutas" in tagalog',
-    'word': 'prutas'
-  },
-]
+document.addEventListener('DOMContentLoaded', () => {
+  // Card Data Array - with our card objects including pair, image, alt & words
+  const cardArray = [
+    {
+      'pair': 1,
+      'image': 'assets/images/card-images/bread-en.png',
+      'alt': 'image of a loaf of bread',
+      'word': 'bread'
+    },
+    {
+      'pair': 1,
+      'image': 'assets/images/card-images/bread-tg.png',
+      'alt': 'image of a loaf of bread; translates to "tinapay" in tagalog',
+      'word': 'tinapay'
+    },
+    {
+      'pair': 2,
+      'image': 'assets/images/card-images/egg-en.png',
+      'alt': 'image of an egg',
+      'word': 'egg'
+    },
+    {
+      'pair': 2,
+      'image': 'assets/images/card-images/egg-tg.png',
+      'alt': 'image of an egg; translates to "itlog" in tagalog',
+      'word': 'itlog'
+    },
+    {
+      'pair': 3,
+      'image': 'assets/images/card-images/fish-en.png',
+      'alt': 'image of a cooked fish',
+      'word': 'fish'
+    },
+    {
+      'pair': 3,
+      'image': 'assets/images/card-images/fish-tg.png',
+      'alt': 'image of a cooked fish; translates to "isda" in tagalog',
+      'word': 'isda'
+    },
+    {
+      'pair': 4,
+      'image': 'assets/images/card-images/fruit-en.png',
+      'alt': 'image of a bowl of fruit',
+      'word': 'fruit'
+    },
+    {
+      'pair': 4,
+      'image': 'assets/images/card-images/fruit-tg.png',
+      'alt': 'image of a bowl of fruit; translates to "prutas" in tagalog',
+      'word': 'prutas'
+    },
+  ]
 
-// Game initialisation, shuffle cards
-cardArray.sort(() => Math.random() - 0.5);
+  // Game initialisation, shuffle cards
+  cardArray.sort(() => Math.random() - 0.5);
 
-// Where cards are 'stored'
-const gridDisplay = document.querySelector('.grid');
-const resultDisplay = document.querySelector('#result');
-let cardsChosen = []; /* Pushing selected cards into array */
-let cardsChosenIds = [];
-let cardsWon = [];
+  // Where cards are 'stored'
+  const gridDisplay = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  let cardsChosen = []; /* Pushing selected cards into array */
+  let cardsChosenIds = [];
+  let cardsWon = [];
 
 
-// Create an element for each item in array
-function createBoard() {
-  for (let i = 0; i < cardArray.length; i++) {
-    const card = document.createElement('img');
-    card.setAttribute('class', 'card');
-    card.setAttribute('src', 'assets/images/card-images/card-reverse.png');
-    card.setAttribute('data-id', i);
-    card.addEventListener('click', flipCard);
-    gridDisplay.appendChild(card);
-  }
-}
-
-createBoard();
-
-function checkMatch() {
-  /* Look for all cards on grid via img tag */
-  const cards = document.querySelectorAll('img');
-  const optionOneId = cardsChosenIds[0];
-  const optionTwoId = cardsChosenIds[1];
-
-  console.log(cards);
-
-  if (optionOneId == optionTwoId) {
-    alert("You have clicked the same image!")
+  // Create an element for item in array
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img');
+      card.setAttribute('class', 'card');
+      card.setAttribute('src', 'assets/images/card-images/card-reverse.png');
+      card.setAttribute('data-id', i);
+      card.addEventListener('click', flipCard);
+      gridDisplay.appendChild(card);
+    }
   }
 
-  console.log("Check for match");
-  if (optionOneId == optionTwoId) {
-    alert('You found a match')
-    cards[optionOneId].setAttribute('src', 'assets/images/card-images/card-match.png');
-    cards[optionTwoId].setAttribute('src', 'assets/images/card-images/card-match.png');
-    cards[optionOneId].removeEventListener('click', flipCard);
-    cards[optionTwoId].removeEventListener('click', flipCard);
-    cardsWon.push(cardsChosen);
-  } else {
-    cards[optionOneId].setAttribute('src', 'assets/images/card-images/card-reverse.png');
-    cards[optionTwoId].setAttribute('src', 'assets/images/card-images/card-reverse.png');
-    alert("Sorry try again!");
+  createBoard();
+
+  function checkMatch() {
+    /* Look for all cards on grid via img tag */
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+
+    console.log(cards);
+
+    if (optionOneId == optionTwoId) {
+      alert("You have clicked the same image!")
+    }
+
+    console.log("Check for match");
+    if (optionOneId == optionTwoId) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'assets/images/card-images/card-match.png');
+      cards[optionTwoId].setAttribute('src', 'assets/images/card-images/card-match.png');
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute('src', 'assets/images/card-images/card-reverse.png');
+      cards[optionTwoId].setAttribute('src', 'assets/images/card-images/card-reverse.png');
+      alert("Sorry try again!");
+    }
+    /* Can also use innerHTML instead of textContent */
+    resultDisplay.textContent = cardsWon.length;
+    cardsChosen = [];
+    cardsChosenIds = [];
+
+    /* Check for if match function */
+
+    if (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = "Congratulations you found them all!"
+    }
   }
-  /* Can also use innerHTML instead of textContent */
-  resultDisplay.textContent = cardsWon.length;
-  cardsChosen = [];
-  cardsChosenIds = [];
 
-  /* Check for if match function */
 
-  if (cardsWon.length === cardArray.length/2) {
-    resultDisplay.textContent = "Congratulations you found them all!"
+  function flipCard() {
+    /* This --> allowing us to interact with the element we click */
+    const cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].pair);
+    cardsChosenIds.push(cardId);
+
+    console.log(cardsChosen);
+    console.log(cardsChosenIds);
+
+    this.setAttribute('src', cardArray[cardId].image);
+    if (cardsChosen.length === 2) {
+      setTimeout( checkMatch, 500)
+    }
   }
-}
-
-
-function flipCard() {
-  /* This --> allowing us to interact with the element we click */
-  const cardId = this.getAttribute('data-id');
-  cardsChosen.push(cardArray[cardId].pair);
-  cardsChosenIds.push(cardId);
-
-  console.log(cardsChosen);
-  console.log(cardsChosenIds);
-
-  this.setAttribute('src', cardArray[cardId].image);
-  if (cardsChosen.length === 2) {
-    setTimeout( checkMatch, 500)
-  }
-}
+  
+})
 
 
 // const results = document.getElementById("results");
